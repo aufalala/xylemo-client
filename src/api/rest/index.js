2917f59ea85d17db932376b66489cb4ce3dbe399
Aufa <aufa_romeo@live.com>
2025-11-06 23:19:42 +0800

    feat: some files and structure

---
+++ src/api/rest/index.js
@@ -0,0 +1,11 @@
+// import * as ordersApi from './orders.js';
+// import * as chatsApi from './chats.js';
+// import * as productsApi from './products.js';
+// import * as buyersApi from './buyers.js';
+
+// export {
+//   ordersApi,
+//   chatsApi,
+//   productsApi,
+//   buyersApi,
+// };
