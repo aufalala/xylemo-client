0c804e58535fa49796f7a4f1c1aaaf084341debd
Aufa <aufa_romeo@live.com>
2025-11-17 00:02:19 +0800

    FEAT: component and css for test chat feed

---
+++ src/components/TestChatFeed.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { useChatFeed } from "../contexts/ChatFeedContext.jsx";
+
+import styles from "./TestChatFeed.module.css"
+
+function TestChatFeed() {
+  const { todaysMessages } = useChatFeed();
+
+  return (
+    <div>
+      <h2>Live Chat Feed</h2>
+      {todaysMessages && todaysMessages.length > 0 && todaysMessages.map((msg) => (
+        <div
+          key={msg._id}
+          className={`${msg.isOrder ? styles.isOrder : ""} ${msg.isFailedOrder ? styles.isFailedOrder : ""} ${msg.isHighlight ? styles.highlight : ""}`}
+        >
+          <strong>{msg.username}:</strong> {msg.text}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default TestChatFeed;
