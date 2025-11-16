4dc78ddb5b37e76a87f252ce38a1850d12b3845d
Aufa <aufa_romeo@live.com>
2025-11-17 00:03:13 +0800

    FEAT: auth and chat context provider in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import { ChatFeedProvider } from "./contexts/ChatFeedContext.jsx";
+import { AuthProvider } from './contexts/authContext.jsx';
+
 import './index.css'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+
+    <AuthProvider>
+      <ChatFeedProvider>
+
+        <App />
+
+      </ChatFeedProvider>
+    </AuthProvider>
+
   </StrictMode>,
 )
